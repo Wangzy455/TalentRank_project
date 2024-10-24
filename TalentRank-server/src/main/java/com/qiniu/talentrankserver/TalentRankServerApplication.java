6623b8f0fbf1d3c28fd10465bc8325172391198c
Wangzy <156842113587@163.com>
2024-10-24 18:55:48 +0800

    初始工程更新

--- TalentRank-server/src/main/java/com/qiniu/talentrankserver/TalentRankServerApplication.java
+++ TalentRank-server/src/main/java/com/qiniu/talentrankserver/TalentRankServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TalentRankServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TalentRankServerApplication.class, args);
     }
-
 }
