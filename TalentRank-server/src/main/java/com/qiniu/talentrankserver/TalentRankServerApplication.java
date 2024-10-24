59536563940ff9699ec27611246d86440b554271
Wangzy <156842113587@163.com>
2024-10-24 18:17:21 +0800

    初始工程

---
+++ TalentRank-server/src/main/java/com/qiniu/talentrankserver/TalentRankServerApplication.java
@@ -0,0 +1,13 @@
+package com.qiniu.talentrankserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentRankServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentRankServerApplication.class, args);
+    }
+
+}
---
+++ TalentRank-server/src/test/java/com/qiniu/talentrankserver/TalentRankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiniu.talentrankserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentRankServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
